bfc4346d7441a1e211b71df6a081e8b5f1b7aa2d
Raffael Queiroga <raffaelqueirogaa@gmail.com>
2025-04-06 10:30:49 -0300

    Concatenacao de Lambda

--- src/problems_solving/exercicios/ConcatenacaoLambda.java
+++ src/problems_solving/exercicios/ConcatenacaoLambda.java
@@ -1,10 +1,19 @@
 package problems_solving.exercicios;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
 // Escreva uma expressão lambda que concatene duas strings.
 public class ConcatenacaoLambda {
     public static void main(String args[]) {
 
+        List<String> palavras = new ArrayList<>(Arrays.asList("Casa", "Carro"));
 
+        Optional<String> resultado = palavras.stream().reduce(String::concat);
+
+        System.out.println(resultado.orElse(""));
 
     }
 }
