f010c6debc1881b62f530465621fedd57e4a53d6
Raffael Queiroga <raffaelqueirogaa@gmail.com>
2025-04-06 10:16:42 -0300

    Quadrado com lambda

--- src/problems_solving/exercicios/ConcatenacaoLambda.java
+++ src/problems_solving/exercicios/ConcatenacaoLambda.java
@@ -2,4 +2,9 @@ package problems_solving.exercicios;
 
 // Escreva uma expressão lambda que concatene duas strings.
 public class ConcatenacaoLambda {
+    public static void main(String args[]) {
+
+
+
+    }
 }
--- src/problems_solving/exercicios/QuadradoComLambda.java
+++ src/problems_solving/exercicios/QuadradoComLambda.java
@@ -1,10 +1,15 @@
 package problems_solving.exercicios;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 // Crie uma função lambda que receba um número inteiro e retorne seu quadrado.
 public class QuadradoComLambda {
+    public static void main(String args[]) {
+        List<Integer> valores = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
 
+        valores.stream().map(n -> n * n).forEach(System.out::println);
 
-
-
-
+    }
 }
