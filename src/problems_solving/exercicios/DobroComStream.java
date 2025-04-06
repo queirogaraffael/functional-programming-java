1ec5161f9d02e4bd4b8f6df25e8bb54162ec64de
Raffael Queiroga <raffaelqueirogaa@gmail.com>
2025-04-06 09:59:51 -0300

    Dobro com Stream

--- src/problems_solving/exercicios/DobroComStream.java
+++ src/problems_solving/exercicios/DobroComStream.java
@@ -1,5 +1,15 @@
 package problems_solving.exercicios;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 // Dado um List<Integer>, use stream para dobrar os valores de todos os elementos.
 public class DobroComStream {
+    public static void main(String args[]) {
+
+        List<Integer> valores = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+
+        valores.stream().map(n -> n * 2).forEach(System.out::println);
+    }
 }
